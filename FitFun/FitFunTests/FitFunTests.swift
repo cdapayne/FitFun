f5777dae7c4616eda7cf1af0df2442b29ee92780
cdapayne <carlton.payne@hotmail.com>
2025-08-29 18:43:08 -0400

    update

---
+++ FitFun/FitFunTests/FitFunTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitFunTests.swift
+//  FitFunTests
+//
+//  Created by PayneBrain on 8/29/25.
+//
+
+import Testing
+@testable import FitFun
+
+struct FitFunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
